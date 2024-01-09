d5e289619faf955e3b5e61ca26eb7b8c0ecdbc0e
thankforcat <ffslmo.wmy@gmail.com>
2024-01-08 20:23:23 -0800

    node20

---
+++ node20.docker.sh
@@ -0,0 +1,20 @@
+# 版本 arm64v8/node:20.10.0-bullseye-slim
+
+apt-get -y update && apt-get -y upgrade && apt-get -y install curl cron wget git zsh nano
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+
+sed -i '/^ZSH_THEME=/c\ZSH_THEME="powerlevel10k/powerlevel10k"' ~/.zshrc
+
+wget https://raw.githubusercontent.com/thankforcat/newlinux/main/.p10k.zsh -O $HOME/.p10k.zsh
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+sed -i '/^plugins=/ s/)/ zsh-autosuggestions)/' ~/.zshrc
+
+# 输出一些信息
+echo "Zsh configuration completed. Reloading shell..."
+source ~/.zshrc
+echo "Shell reloaded."

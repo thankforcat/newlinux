4c2390d3e81a9fc28b6f402073f48d252164c112
thankforcat <ffslmo.wmy@gmail.com>
2024-01-08 20:46:24 -0800

    2

--- node20.docker.sh
+++ node20.docker.sh
@@ -1,5 +1,5 @@
 # 版本 arm64v8/node:20.10.0-bullseye-slim
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 
